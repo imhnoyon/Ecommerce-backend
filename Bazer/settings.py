6ea008ad6f1fe2546c3e2218a16a59d204e4933b
ihmnoyon <noyonhasan320@gmail.com>
2026-01-06 12:27:39 +0600

    Ready to deploy

--- Bazer/settings.py
+++ Bazer/settings.py
@@ -89,12 +89,16 @@ WSGI_APPLICATION = 'Bazer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME':env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
